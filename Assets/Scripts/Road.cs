14aa25169163d9b96ae3cfeee9eb6c417a10219f
hahw0627 <hahw0627@naver.com>
2024-06-16 00:59:20 +0900

    Add : CarClone
    길에 자동차 생성 추가

--- Assets/Scripts/Road.cs
+++ Assets/Scripts/Road.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Road : MonoBehaviour
 {
+
+    public Car CloneTarget = null;
+    public Transform GenerationPos = null;
+
+    public int GenerationPersent = 50;
+
+    public float CloneDelaySec = 1.0f;
+
+    protected float NextSecToClone = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,25 @@ public class Road : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float currSec = Time.time;
+        if(NextSecToClone <= currSec)
+        {
+            int randomval = Random.Range(0, 100);
+            if(randomval <= GenerationPersent)
+            {
+                CloneCar();
+            }
+            NextSecToClone = currSec + CloneDelaySec;
+        }
+    }
+
+    void CloneCar()
+    {
+        Transform clonepos = GenerationPos;
+        Vector3 offsetpos = clonepos.position;
+        offsetpos.y = 1.0f;
+
+        GameObject cloneobj = GameObject.Instantiate(CloneTarget.gameObject, offsetpos, GenerationPos.rotation, this.transform);
+        cloneobj.SetActive(true);
     }
 }
